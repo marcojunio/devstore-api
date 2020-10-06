9ab306a25c382803b424ef1c85ed2e0391a92c5e
marco.junio <marcos.junior@viannasempre.com.br>
2020-10-06 14:59:06 -0300

    estudo de api com WebApi

---
+++ DevStore/DevStore.Domain/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DevStore.Domain
+{
+    public class Category
+    {
+        public int? Id{ get; set; }
+        public string Title { get; set; }
+        public override string ToString()
+        {
+            return this.Title;
+        }
+
+    }
+
+
+}
---
+++ DevStore/DevStore.Domain/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DevStore.Domain
+{
+    public class Product
+    {
+        public Product()
+        {
+            this.AcquireDate = DateTime.Now;
+        }
+        public int? Id { get; set; }
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+        public DateTime AcquireDate { get; set; }
+        public  bool IsActive{ get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+        public override string ToString()
+        {
+            return this.Title;
+        }
+    }   
+}
